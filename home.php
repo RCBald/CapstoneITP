a2aaa4e118a5d2b411b1a73dfe6e87f486fabac5
RCBald <rcbaldwin77@gmail.com>
2024-09-18 18:22:11 -0400

    Creating php pages

---
+++ home.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#public">Public</a>
+        <a href="#private">Private</a>
+    </div>
+</head>
+<body>
+</body>
+</html>
