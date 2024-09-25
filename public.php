ccaec7fb37b0e1eb1ef088d1e1a183be927f6ba3
RCBald <rcbaldwin77@gmail.com>
2024-09-25 13:47:14 -0400

    Setting up table + adding imgs and img folder

--- public.php
+++ public.php
@@ -4,7 +4,7 @@
         <link rel="stylesheet" href="css/style.css">
         <Title> Public</Title>
     </head>
-    <di>
+    <body>
         <div class="Public_title">
             <h1> Public Page </h1>
         </div>
@@ -15,5 +15,46 @@
                     <li><a href="private.php">Private</a></li>   
             </ul>
         </nav>
+        <h4>Fake Table Info</h4>
+        <table>
+            <tr>
+                <th>Dish Name</th>
+                <th>Dish Price</th>
+                <th>Dish Category</th>
+                <th>Dish Description</th>
+                <th>Image</th>
+            </tr>
+            <tr>
+                <td>Calamari</td>
+                <td>20.00</td>
+                <td>App</td>
+                <td>Fried Squid</td>
+
+            </tr>
+            <tr>
+                <td>Clam Chowder</td>
+                <td>10.00</td>
+                <td>App</td>
+                <td> soup with Clam</td>
+            </tr>
+            <tr>
+                <td>Ribeye</td>
+                <td>100.00</td>
+                <td>Entre</td>
+                <td>Big honkin steak</td>
+            </tr>
+            <tr>
+                <td>Penne Parmgiana</td>
+                <td>30.00</td>
+                <td>Entre</td>
+                <td> Pasta with red sauce</td>
+            </tr>
+            <tr>
+                <td>American Burger</td>
+                <td>15.00</td>
+                <td>Entre</td>
+                <td> Best burger in town</td>
+            </tr>
+        </table>
     </body>
 </html>
